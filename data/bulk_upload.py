ff457f901eee78e31905ddfdb658662d951aed26
John Tran <john.kn.tran@gmail.com>
2018-03-31 12:52:23 -0400

    Processed bulk upload data in script.

--- data/bulk_upload.py
+++ data/bulk_upload.py
@@ -8,8 +8,15 @@ with open(r'data.json') as f:
         text += line
     data = json.loads(text)
 
+def escape_double_quotes(text):
+    return text.replace('"', '\"')
+
 with open(r'bulk.json', 'wb') as g:
     for d in data:
         g.write('{ "index": {"_index": "quotes", "_type": "doc"} }\n')
-        g.write('< "author": {}, "quote": {} >\n'.format(d['author'], d['quote']).replace('<', '{').replace('>', '}'))
+        g.write('< "author": "{}", "quote": "{}" >\n'.format(
+            escape_double_quotes(d['author']),
+            escape_double_quotes(d['quote']))
+                .replace('<', '{').replace('>', '}')
+        )
         
